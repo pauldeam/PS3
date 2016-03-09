6b40668817853a0dba513274dc251972889e2904
Paul Jureidini <pauldeam@udel.edu>
2016-03-09 17:21:45 -0500

    secondary commit brufa

--- PS3/src/main/java/PS3/Account.java
+++ PS3/src/main/java/PS3/Account.java
@@ -1,7 +1,6 @@
 package PS3;
 
 import java.util.Date;
-import java.util.Scanner;
 
 public class Account {
 	
@@ -10,20 +9,78 @@ public class Account {
 	private static double annualInterestRate;
 	private static Date dateCreated;
 	
-	Scanner scan = new Scanner(System.in);
-	
-	public static void defaultAcount()
+	/**
+	 *  Creates a no arg constructor with default values (0's) for the account
+	 */
+	public void defaultAcount()
 	{
 		id = 0;
 		balance = 0;
 		annualInterestRate = 0;
-//		dateCreated;
-				
+		dateCreated = new Date();
 	}
 	
-	public static void Account()
+	/**
+	 * Creates account with specified values 
+	 * @param id
+	 * @param balance
+	 */
+	public Account(int id, double balance)
 	{
-		System.out.println("Please enter in ");
+		id = id;
+		balance = balance;
+		
+	}
+
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static int getId() {
+		return id;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static void setId(int id) {
+		Account.id = id;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static double getBalance() {
+		return balance;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static void setBalance(double balance) {
+		Account.balance = balance;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static void setAnnualInterestRate(double annualInterestRate) {
+		Account.annualInterestRate = annualInterestRate;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static Date getDateCreated() {
+		return dateCreated;
 	}
 
 }
