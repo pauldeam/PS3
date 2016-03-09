35b1e9df1bcde4d651da49812c6d380a45a6083c
Paul Jureidini <pauldeam@udel.edu>
2016-03-09 16:52:37 -0500

    initial commit

---
+++ PS3/src/main/java/PS3/Account.java
@@ -0,0 +1,29 @@
+package PS3;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Account {
+	
+	private static int id;
+	private static double balance;
+	private static double annualInterestRate;
+	private static Date dateCreated;
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public static void defaultAcount()
+	{
+		id = 0;
+		balance = 0;
+		annualInterestRate = 0;
+//		dateCreated;
+				
+	}
+	
+	public static void Account()
+	{
+		System.out.println("Please enter in ");
+	}
+
+}
