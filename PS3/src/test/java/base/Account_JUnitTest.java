bc06c3812514ebd76ad70b97adbe93ded57fdb20
Paul Jureidini <pauldeam@udel.edu>
2016-03-09 23:15:23 -0500

    no idea whats wrong but im done

--- PS3/src/test/java/base/Account_JUnitTest.java
+++ PS3/src/test/java/base/Account_JUnitTest.java
@@ -3,11 +3,12 @@ package base;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import PS3.InsufficientFundsException;
 import PS3.Account;
+import PS3.InsufficientFundsException;
 
 
-public class Account_JUnitTest {
+public class Account_JUnitTest 
+{
 
 	@Test
 	public void test() throws InsufficientFundsException
@@ -15,7 +16,13 @@ public class Account_JUnitTest {
 		Account.realAccount(1122, 20000.00);
 		Account.setAnnualInterestRate(4.5);
 		Account.deposit(3000.00);
-		Account.withdraw(2500.00);
+		try {
+			Account.withdraw(2500.00);
+		} catch (InsufficientFundsException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			System.out.println("You do not have enough funds in your account to withdraw this amount.");
+		}
 		System.out.println("ACCOUNT BALANCE: " + Account.getBalance() +
 				"\nMONTHLY INTEREST: " + Account.monthlyInterestRate() +
 				"\nDATE: " + Account.getDateCreated());
